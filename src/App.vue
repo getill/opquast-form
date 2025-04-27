fc683ed4ff92dd3a91b5c6d9126ca86dbb01f460
getill <theo.lemray@gmail.com>
2025-04-27 18:19:07 +0200

    build created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-import Nav from "./components/Nav.vue";
-import Form from './components/Form.vue'
-import Footer from "./components/Footer.vue";
+import Nav from './components/Nav.vue';
+import Form from './components/Form.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
+  <div class="app-container">
+
     <Nav />
     <main>
       <Form />
     </main>
     <Footer />
+  </div>
 </template>
 
 <style scoped>
